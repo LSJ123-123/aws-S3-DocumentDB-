c81fdff1e547755ce3c83e186891804b66a9d2f8
LSJ123-123 <113840393+LSJ123-123@users.noreply.github.com>
2025-02-03 00:22:17 +0900

    Update route.ts
    추후 업데이터 가능성?

--- icon/app/api/update-icons/route.ts
+++ icon/app/api/update-icons/route.ts
@@ -1,5 +1,6 @@
 // // app/api/update-icons/route.ts
 // // 사용 안 함.....
+// 기능을 좀 더 수행해보고 업데이트 해보겠습니다.
 // import { NextResponse } from 'next/server';
 // import { updateIconPaths } from '@/app/lib/updateIconPaths';
 
